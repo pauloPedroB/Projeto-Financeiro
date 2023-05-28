16478058bcf325b34fa3a8c42bee1b1cf50e94be
ARTHUR VERDADEIRO <arthur.verdadeiro@etec.sp.gov.br>
2023-05-28 19:20:14 -0300

    Adição da logo criada,mudança para teste graficos!

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <img style="margin-left:40%; margin-right: 50%; width: 20%; height: 20%;" src="/images/FineX - Name.png" alt="FineX - Logo">
         </x-slot>
 
         <x-validation-errors class="mb-4" />
--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <title>Document</title>
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+    <title>FineX - Gráficos Teste</title>
 </head>
 <body>
 @auth
